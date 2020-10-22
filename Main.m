c70fdb2e4188442fff79d0a7d5c83f7ed4e64922
Jingwei Too <30698779+JingweiToo@users.noreply.github.com>
2020-10-22 22:03:12 +0800

    Add files via upload
    Improve code for the fitness function

--- Main.m
+++ Main.m
@@ -17,7 +17,6 @@
 % N4:     Number of trees in fourth group
 % theta:  Tree reduction rate
 % lambda: Parameter controls nearest tree
-% *Note: k-value of KNN & hold-out setting can be modified in jFitnessFunction.m
 %---Outputs----------------------------------------------------------------
 % sFeat:  Selected features
 % Sf:     Selected feature index
@@ -31,10 +30,14 @@
 clc, clear, close 
 % Benchmark data set 
 load ionosphere.mat; 
+% Set 20% data as validation set
+ho=0.2; 
+% Hold-out method
+HO=cvpartition(label,'HoldOut',ho,'Stratify',false);
 % Parameter setting
 N=10; T=100; N1=3; N2=5; N4=3; theta=0.8; lambda=0.5; 
 % Binary Tree Growth Algorithm 
-[sFeat,Sf,Nf,curve]=jBTGA(feat,label,N,T,N1,N2,N4,theta,lambda);
+[sFeat,Sf,Nf,curve]=jBTGA(feat,label,N,T,N1,N2,N4,theta,lambda,HO);
 % Plot convergence curve
 figure(); plot(1:T,curve); xlabel('Number of iterations');
 ylabel('Fitness Value'); title('BTGA'); grid on;
--- jBTGA.m
+++ jBTGA.m
@@ -1,29 +1,4 @@
-%-------------------------------------------------------------------------%
-%  Binary Tree Growth Algorithm (BTGA) source codes demo version          %
-%                                                                         %
-%  Programmer: Jingwei Too                                                %
-%                                                                         %
-%  E-Mail: jamesjames868@gmail.com                                        %
-%-------------------------------------------------------------------------%
-
-function [sFeat,Sf,Nf,curve]=jBTGA(feat,label,N,T,N1,N2,N4,theta,lambda)
-%---Inputs-----------------------------------------------------------------
-% feat:   features
-% label:  labelling
-% N:      Number of trees
-% T:      Maximum number of iterations
-% N1:     Number of trees in first group
-% N2:     Number of trees in second group
-% N4:     Number of trees in fourth group
-% theta:  Tree reduction rate
-% lambda: Parameter controls nearest tree
-%---Outputs----------------------------------------------------------------
-% sFeat:  Selected features
-% Sf:     Selected feature index
-% Nf:     Number of selected features
-% curve:  Convergence curve
-%--------------------------------------------------------------------------
-
+function [sFeat,Sf,Nf,curve]=jBTGA(feat,label,N,T,N1,N2,N4,theta,lambda,HO)
 
 if N4 > N1+N2, N4=N1+N2; end
 fun=@jFitnessFunction; 
@@ -37,16 +12,14 @@ for i=1:N
 end
 fit=zeros(1,N); fitG=inf; 
 for i=1:N
-  fit(i)=fun(feat,label,X(i,:));
+  fit(i)=fun(feat,label,X(i,:),HO);
   if fit(i) < fitG
     fitG=fit(i); Xgb=X(i,:);
   end
 end
 [fit,idx]=sort(fit,'ascend'); X=X(idx,:); 
-Xnew=zeros(N4,D); Fnew=zeros(1,N4); dist=zeros(1,N1+N2); X1=zeros(N1,D);
-curve=inf; t=1; 
-figure(1); clf; axis([1 100 0 0.2]); xlabel('Number of iterations');
-ylabel('Fitness Value'); title('Convergence Curve'); grid on;
+Xnew=zeros(N4,D); Fnew=zeros(1,N4); dist=zeros(1,N1+N2); 
+X1=zeros(N1,D); curve=inf; t=1; 
 %---Iteration start-------------------------------------------------------
 while t <= T
   for i=1:N1
@@ -60,7 +33,7 @@ while t <= T
         X1(i,d)=0;
       end
     end
-    fitT=fun(feat,label,X1(i,:)); 
+    fitT=fun(feat,label,X1(i,:),HO); 
     if fitT <= fit(i)
       X(i,:)=X1(i,:); fit(i)=fitT;
     end
@@ -86,13 +59,13 @@ while t <= T
         X(i,d)=0;
       end
     end
-    fit(i)=fun(feat,label,X(i,:));
+    fit(i)=fun(feat,label,X(i,:),HO);
   end
   for i=N1+N2+1:N
     for d=1:D
       X(i,d)=randi([0,1]);
     end
-    fit(i)=fun(feat,label,X(i,:));
+    fit(i)=fun(feat,label,X(i,:),HO);
   end
   for i=1:N4
     r=randi([1,N1]); Xbest=X(r,:);
@@ -105,7 +78,7 @@ while t <= T
         Xnew(i,d)=Xn;
       end
     end
-    Fnew(i)=fun(feat,label,Xnew(i,:));
+    Fnew(i)=fun(feat,label,Xnew(i,:),HO);
   end
   XX=[X;Xnew]; FF=[fit,Fnew]; [FF,idx]=sort(FF,'ascend');
   X=XX(idx(1:N),:); fit=FF(1:N);
@@ -113,8 +86,7 @@ while t <= T
     fitG=fit(1); Xgb=X(1,:);
   end
   curve(t)=fitG; 
-  pause(1e-20); hold on;
-  CG=plot(t,fitG,'Color','r','Marker','.'); set(CG,'MarkerSize',5);
+  fprintf('\nIteration %d Best (BTGA)= %f',t,curve(t))
   t=t+1;
 end
 Pos=1:D; Sf=Pos(Xgb==1); Nf=length(Sf); sFeat=feat(:,Sf); 
