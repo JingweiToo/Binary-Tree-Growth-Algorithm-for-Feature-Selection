53e29d65c15e7c8042fcb8249a0cc8aee49dafb3
Jingwei Too <30698779+JingweiToo@users.noreply.github.com>
2020-12-20 10:28:24 +0800

    Add files via upload

--- Main.m
+++ Main.m
@@ -1,47 +1,53 @@
-%-------------------------------------------------------------------------%
-%  Binary Tree Growth Algorithm (BTGA) source codes demo version          %
-%                                                                         %
-%  Programmer: Jingwei Too                                                %
-%                                                                         %
-%  E-Mail: jamesjames868@gmail.com                                        %
-%-------------------------------------------------------------------------%
+%-------------------------------------------------------------------%
+%  Binary Tree Growth Algorithm (BTGA) demo version                 %
+%-------------------------------------------------------------------%
 
 
-%---Inputs-----------------------------------------------------------------
-% feat:   features
-% label:  labelling
-% N:      Number of trees
-% T:      Maximum number of iterations
-% N1:     Number of trees in first group
-% N2:     Number of trees in second group
-% N4:     Number of trees in fourth group
-% theta:  Tree reduction rate
-% lambda: Parameter controls nearest tree
-%---Outputs----------------------------------------------------------------
-% sFeat:  Selected features
-% Sf:     Selected feature index
-% Nf:     Number of selected features
-% curve:  Convergence curve
-%--------------------------------------------------------------------------
+%---Input------------------------------------------------------------
+% feat     : feature vector (instances x features)
+% label    : label vector (instances x 1)
+% N        : Number of trees
+% max_Iter : Maximum number of iterations
+% N1       : Number of trees in first group
+% N2       : Number of trees in second group
+% N4       : Number of trees in fourth group
+% theta    : Tree reduction rate
+% lambda   : Parameter controls nearest tree
 
+%---Output-----------------------------------------------------------
+% sFeat    : Selected features (instances x features)
+% Sf       : Selected feature index
+% Nf       : Number of selected features
+% curve    : Convergence curve
+%--------------------------------------------------------------------
 
 
 %% Binary Tree Growth Algorithm  
 clc, clear, close 
 % Benchmark data set 
 load ionosphere.mat; 
-% Set 20% data as validation set
-ho=0.2; 
-% Hold-out method
-HO=cvpartition(label,'HoldOut',ho,'Stratify',false);
-% Parameter setting
-N=10; T=100; N1=3; N2=5; N4=3; theta=0.8; lambda=0.5; 
-% Binary Tree Growth Algorithm 
-[sFeat,Sf,Nf,curve]=jBTGA(feat,label,N,T,N1,N2,N4,theta,lambda,HO);
-% Plot convergence curve
-figure(); plot(1:T,curve); xlabel('Number of iterations');
-ylabel('Fitness Value'); title('BTGA'); grid on;
 
+% Set 20% data as validation set
+ho = 0.2; 
+% Hold-out method
+HO = cvpartition(label,'HoldOut',ho,'Stratify',false);
+
+% Parameter setting
+N        = 10;
+max_Iter = 100;
+N1       = 3; 
+N2       = 5;
+N4       = 3;
+theta    = 0.8;
+lambda   = 0.5; 
+% Binary Tree Growth Algorithm 
+[sFeat,Sf,Nf,curve] = jBTGA(feat,label,N,max_Iter,N1,N2,N4,theta,lambda,HO);
+
+% Plot convergence curve
+plot(1:max_Iter,curve);
+xlabel('Number of iterations');
+ylabel('Fitness Value');
+title('BTGA'); grid on;
 
 
 
--- jBTGA.m
+++ jBTGA.m
@@ -1,94 +1,119 @@
-function [sFeat,Sf,Nf,curve]=jBTGA(feat,label,N,T,N1,N2,N4,theta,lambda,HO)
+function [sFeat,Sf,Nf,curve] = jBTGA(feat,label,N,max_Iter,N1,N2,...
+  N4,theta,lambda,HO)
 
-if N4 > N1+N2, N4=N1+N2; end
-fun=@jFitnessFunction; 
-D=size(feat,2); X=zeros(N,D);
-for i=1:N
-  for d=1:D
+if N4 > N1+N2
+  N4 = N1 + N2;
+end
+fun = @jFitnessFunction; 
+dim = size(feat,2);
+X   = zeros(N,dim);
+for i = 1:N
+  for d = 1:dim
     if rand() > 0.5
-      X(i,d)=1;
+      X(i,d) = 1;
     end
   end
 end
-fit=zeros(1,N); fitG=inf; 
-for i=1:N
-  fit(i)=fun(feat,label,X(i,:),HO);
+fit  = zeros(1,N);
+fitG = inf; 
+for i = 1:N
+  fit(i) = fun(feat,label,X(i,:),HO);
   if fit(i) < fitG
-    fitG=fit(i); Xgb=X(i,:);
+    fitG = fit(i);
+    Xgb  = X(i,:);
   end
 end
-[fit,idx]=sort(fit,'ascend'); X=X(idx,:); 
-Xnew=zeros(N4,D); Fnew=zeros(1,N4); dist=zeros(1,N1+N2); 
-X1=zeros(N1,D); curve=inf; t=1; 
-%---Iteration start-------------------------------------------------------
-while t <= T
-  for i=1:N1
-    r1=rand();
-    for d=1:D
-      Xn=X(i,d)/theta+r1*X(i,d);
-      S=1/(1+exp(-Xn));
+[fit, idx] = sort(fit,'ascend');
+X    = X(idx,:); 
+Xnew = zeros(N4, dim);
+Fnew = zeros(1, N4);
+dist = zeros(1, N1 + N2); 
+X1   = zeros(N1, dim);
+
+curve = inf; 
+t = 1; 
+%---Iteration start-------------------------------------------------
+while t <= max_Iter
+  for i = 1:N1
+    r = rand();
+    for d = 1:dim
+      Xn = X(i,d) / theta + r * X(i,d);
+      S  = 1 / (1 + exp(-Xn));
       if rand() < S
-        X1(i,d)=1;
+        X1(i,d) = 1;
       else
-        X1(i,d)=0;
+        X1(i,d) = 0;
       end
     end
-    fitT=fun(feat,label,X1(i,:),HO); 
+    fitT = fun(feat,label,X1(i,:),HO); 
     if fitT <= fit(i)
-      X(i,:)=X1(i,:); fit(i)=fitT;
+      X(i,:) = X1(i,:);
+      fit(i) = fitT;
     end
   end
-  Xorg=X;
-  for i=N1+1:N1+N2
-    for j=1:N1+N2
-      if j~=i
-        dist(j)=sqrt(sum((Xorg(j,:)-Xorg(i,:)).^2));
+  X_ori = X;
+  for i = N1 + 1 : N1 + N2
+    for j = 1 : N1 + N2
+      if j ~= i
+        dist(j) = sqrt(sum((X_ori(j,:) - X_ori(i,:)) .^ 2));
       else
-        dist(j)=inf;
+        dist(j) = inf;
       end
     end
-    [~,idx2]=sort(dist,'ascend'); T1=Xorg(idx2(1),:); T2=Xorg(idx2(2),:);
-    alpha=rand();
-    for d=1:D
-      Y=lambda*T1(d)+(1-lambda)*T2(d);
-      Xn=X(i,d)+alpha*Y;
-      S=1/(1+exp(-Xn));
+    [~, idx_T] = sort(dist,'ascend'); 
+    T1    = X_ori(idx_T(1),:); 
+    T2    = X_ori(idx_T(2),:);
+    alpha = rand();
+    for d = 1:dim
+      Y  = lambda * T1(d) + (1 - lambda) * T2(d);
+      Xn = X(i,d) + alpha * Y;
+      S  = 1 / (1 + exp(-Xn));
       if rand() < S
-        X(i,d)=1;
+        X(i,d) = 1;
       else
-        X(i,d)=0;
+        X(i,d) = 0;
       end
     end
-    fit(i)=fun(feat,label,X(i,:),HO);
+    fit(i) = fun(feat,label,X(i,:),HO);
   end
-  for i=N1+N2+1:N
-    for d=1:D
+  for i = N1 + N2 + 1 : N
+    for d=1:dim
       X(i,d)=randi([0,1]);
     end
     fit(i)=fun(feat,label,X(i,:),HO);
   end
   for i=1:N4
-    r=randi([1,N1]); Xbest=X(r,:);
-    mask=randi([0,1],1,D);
-    for d=1:D
-      Xn=randi([0,1]);
-      if mask(d)==1
-        Xnew(i,d)=Xbest(d);
-      elseif mask(d)==0
-        Xnew(i,d)=Xn;
+    idx_B = randi([1,N1]);
+    Xbest = X(idx_B,:);
+    mask  = randi([0,1],1,dim);
+    for d = 1:dim
+      Xn = randi([0,1]);
+      if mask(d) == 1
+        Xnew(i,d) = Xbest(d);
+      elseif mask(d) == 0
+        Xnew(i,d) = Xn;
       end
     end
-    Fnew(i)=fun(feat,label,Xnew(i,:),HO);
+    Fnew(i) = fun(feat,label,Xnew(i,:),HO);
   end
-  XX=[X;Xnew]; FF=[fit,Fnew]; [FF,idx]=sort(FF,'ascend');
-  X=XX(idx(1:N),:); fit=FF(1:N);
+  XX  = [X; Xnew]; 
+  FF  = [fit, Fnew]; 
+  [FF, idx] = sort(FF,'ascend');
+  X   = XX(idx(1:N),:);
+  fit = FF(1:N);
   if fit(1) < fitG
-    fitG=fit(1); Xgb=X(1,:);
+    fitG = fit(1);
+    Xgb  = X(1,:);
   end
-  curve(t)=fitG; 
+  curve(t) = fitG; 
   fprintf('\nIteration %d Best (BTGA)= %f',t,curve(t))
-  t=t+1;
+  t = t + 1;
 end
-Pos=1:D; Sf=Pos(Xgb==1); Nf=length(Sf); sFeat=feat(:,Sf); 
+Pos   = 1:dim; 
+Sf    = Pos(Xgb == 1);
+Nf    = length(Sf); 
+sFeat = feat(:,Sf); 
 end
+
+
 
